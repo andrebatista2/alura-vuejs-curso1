a8c2aa31413f726a5175520f8ab577a4d692128f
andrebatista2 <andrelucas.batista@outlook.com>
2024-01-30 16:20:46 -0400

    Adicionados comentários relevantes

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -58,42 +58,13 @@ import { getCategories } from "@/http";
 import type ICategories from "@/interfaces/ICategories";
 
 export default {
-  /*
-    Explicação sobre os lifecycle hooks: https://www.alura.com.br/artigos/vuejs-ciclo-vida-componentes?_gl=1*p0qe3g*_ga*MTM4NjA0MTIxNS4xNzAxMzQ2OTUx*_ga_1EPWSW3PCS*MTcwNjYzNDkxOS4yMi4xLjE3MDY2MzY5NTguMC4wLjA.*_fplc*JTJGTEVGNm1YRjJMeiUyRnVvRWF3R2NTNkc5aWdtdG1rUjlaWFp3JTJGb0ZiVG5NcG5IUXZuaVdQSzZKN0FTV3gweWxDcndndFdCcU1MWHJOV3JVRVMwUTk4SjlRRFQyZnc0b2gxTjNRRFltckxBY0NwN2QlMkZXZDFhYlg4bXVJNFQ3S2clM0QlM0Q.
-  */
   data() {
     return {
       options: [] as ICategories[]
     }
   },
-  /*
-    O beforeCreate é executado imediatamente assim que um componente é inicializado. Nesse momento, estados e
-    propriedades computadas ainda não foram processadas.
-  */
-  beforeCreate() {
-    alert('Sou executado no momento da inicialização do componente. Ideal para checar, por exemplo, se um usuário está autenticado')
-  },
-  /*
-    O created é executado antes do componente ser montado no DOM, porém, nessa etapa todos os estados,
-    propriedades computadas, métodos e watchers já foram processados e estão prontos para serem acessados.
-
-    Esse é um hook muito útil para fazer uma requisição HTTP para uma API e atualizar o estado do componente
-    com esses dados, que serão renderizados no DOM.
-
-    Ele é acessado depois de data() ter sido definido, podendo acessa-las e modifica-las
-  */
   async created() {
     this.options = await getCategories();
   },
-  /*
-    O mounted é executado imediatamente após o componente ser montado. Nesse estágio, o componente se
-    torna funcional: as propriedades de data() são injetadas no template, os elementos são renderizados
-    e a manipulação do DOM se torna possível.
-
-    Só funcionará após a renderização do componente, e assim que o data e o created forem executados
-  */
-  mounted() {
-    console.log('Tudo terminou de renderizar, eu apareço')
-  }
 }
 </script>
